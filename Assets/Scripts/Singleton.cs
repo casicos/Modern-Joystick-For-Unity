e624664a2f87612b6a6f0413eca45022c8e4ddd0
Carey Jo <casicos94@gmail.com>
2018-10-16 16:11:00 +0900

    Update Singleton.cs
    *Fix the initialize problem to attributes of singleton classes

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -1,19 +1,24 @@
-﻿using UnityEngine;
-
-public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
-    private static T _instance = null;
+    
+    private static T _instance;
 
-    public static T GetInstance { get { return _instance ? _instance : GetInstanceObject(); } }
+    public static T GetInstance => _instance ? _instance : GetInstanceObject();
 
     protected virtual void Awake()
     {
-        _instance = this as T;
+        _instance = GetInstanceObject();
     }
+
+    protected virtual void Initialize(){}
 
     private static T GetInstanceObject()
     {
+        if (_instance != null) return _instance;
+        
         _instance = FindObjectOfType<T>().GetComponent<T>();
+        _instance.GetComponent<Singleton<T>>().Initialize();
+
         return _instance;
     }
         
